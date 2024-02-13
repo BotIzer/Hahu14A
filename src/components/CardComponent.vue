0479f68d653bf7888ca0bd7eafbf609b1ac6e65c
BotIzer <izerboti@gmail.com>
2024-02-13 13:49:33 +0100

    card component imported

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-card bordered class="my-card" flat>
+    <q-card-section>
+      <div class="text-h6">Our Changing Planet</div>
+    </q-card-section>
+
+    <q-card-section class="q-pt-none">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua.
+    </q-card-section>
+
+    <q-separator inset />
+
+    <q-card-section>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua.
+    </q-card-section>
+  </q-card>
+</template>
--- src/pages/EmptyPage.vue
+++ src/pages/EmptyPage.vue
@@ -1,23 +1,10 @@
 <script setup lang="ts">
-
-// import { useStore } from "src/stores/store";
-// import { onMounted } from "vue";
-// import { date } from "quasar";
-// import router from "src/router";
-
-// const store = useStore();
-
-// onMounted(() => {
-//   store.many_GetAll();
-// });
-
+import CardComponent from "src/components/CardComponent.vue";
 </script>
 
 <template>
   <q-page class="q-pa-xl">
-    <h1>árvíztűrőtükörfúrógép</h1>
-    <h1>ÁRVÍZTŰRŐTÜKÖRFÚRÓGÉP</h1>
-    <q-btn class="q-px-xs-sm" color="primary" glossy icon="mdi-mail" label="OK" no-caps stack to="/" />
+    <CardComponent></CardComponent>
   </q-page>
 </template>
 

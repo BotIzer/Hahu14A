2b7bfeb57bed3ad72dc2b1cb6ece439172216c26
BotIzer <izerboti@gmail.com>
2024-02-18 11:43:28 +0100

    Picker field created

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -10,38 +10,36 @@ export default {
 };
 </script>
 <template>
-  <div class="col-sm-12 col-md-6 col-l-4 col-xl-3">
-    <q-card bordered flat>
-      <q-card-section class="text-center text-h5" style="background-color: rgb(200, 190, 156)">
-        Our Changing Planet
-      </q-card-section>
-      <q-card-section class="text-h7" style="background-color: rgb(255, 228, 196)">
-        <ul>
-          <li><span>Szin: </span><b>template</b></li>
-          <li><span>Évjárat: </span><b>template</b></li>
-          <li><span>Hengerűrtartalom: </span><b>template</b></li>
-          <li><span>Hirdetés dátuma: </span><b>template</b></li>
-        </ul>
-      </q-card-section>
-      <q-card-section class="" style="background-color: rgb(200, 190, 156)">
-        <div class="text-h7 text-justify">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua.
-        </div>
-        <hr />
-        <q-toggle v-model="template" label="Teljes hirdetés" />
-      </q-card-section>
-      <q-card-section style="background-color: rgb(255, 228, 196)">
-        <q-img role="img" src="../assets/logo.png" style="max-height: 200px"></q-img>
-      </q-card-section>
-      <q-card-section style="background-color: rgb(200, 190, 156)">
-        <div class="text-h7 text-justify">link</div>
-      </q-card-section>
-      <q-card-actions class="justify-center" style="background-color: rgb(255, 228, 196)">
-        <q-btn class="bg-green-3" label="Hirdetés szerkesztése" no-caps type="button"> </q-btn>
-      </q-card-actions>
-    </q-card>
-  </div>
+  <q-card bordered flat>
+    <q-card-section class="text-center text-h5" style="background-color: rgb(200, 190, 156)">
+      Our Changing Planet
+    </q-card-section>
+    <q-card-section class="text-h7" style="background-color: rgb(255, 228, 196)">
+      <ul>
+        <li><span>Szin: </span><b>template</b></li>
+        <li><span>Évjárat: </span><b>template</b></li>
+        <li><span>Hengerűrtartalom: </span><b>template</b></li>
+        <li><span>Hirdetés dátuma: </span><b>template</b></li>
+      </ul>
+    </q-card-section>
+    <q-card-section class="" style="background-color: rgb(200, 190, 156)">
+      <div class="text-h7 text-justify">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua.
+      </div>
+      <hr />
+      <q-toggle v-model="template" label="Teljes hirdetés" />
+    </q-card-section>
+    <q-card-section style="background-color: rgb(255, 228, 196)">
+      <q-img role="img" src="../assets/logo.png" style="max-height: 200px"></q-img>
+    </q-card-section>
+    <q-card-section style="background-color: rgb(200, 190, 156)">
+      <div class="text-h7 text-justify">link</div>
+    </q-card-section>
+    <q-card-actions class="justify-center" style="background-color: rgb(255, 228, 196)">
+      <q-btn class="bg-green-3" label="Hirdetés szerkesztése" no-caps type="button"> </q-btn>
+    </q-card-actions>
+  </q-card>
 </template>
 
 <style lang="sass" scoped></style>
--- src/pages/EmptyPage.vue
+++ src/pages/EmptyPage.vue
@@ -3,9 +3,12 @@ import CardComponent from "src/components/CardComponent.vue";
 </script>
 
 <template>
-  <q-page class="q-pa-xl">
+  <q-page>
+    <div class="row justify-center"></div>
     <div class="row justify-center q-ma-xl">
-      <CardComponent></CardComponent>
+      <div class="col-sm-12 col-md-6 col-l-4 col-xl-3">
+        <CardComponent></CardComponent>
+      </div>
     </div>
   </q-page>
 </template>
